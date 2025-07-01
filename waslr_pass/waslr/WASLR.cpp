db7a6cffa22e294e9a5d16c57223e2e1d19981b0
Noah Kappert <noah.kappert@uni-due.de>
2025-07-01 13:01:05 +0200

    New approach for Stack randomization and first part of heap randomization

--- waslr_pass/waslr/WASLR.cpp
+++ waslr_pass/waslr/WASLR.cpp
@@ -18,10 +18,10 @@ using namespace llvm;
 PreservedAnalyses WASLR::run(Module &M, ModuleAnalysisManager &AM) {
   outs() << "Running WASLR Pass on " << M.getName() << "\n";
   outs() << "Globals Addr Space: " << M.getDataLayout().getDefaultGlobalsAddressSpace() << "\n";
-  for (Function &F : M) {
+  /*for (Function &F : M) {
     waslr::handleFunction(F);
-  }
-  waslr::randomizeStackBase(M);
+  }*/
+  waslr::randomize(M);
   return PreservedAnalyses::all();
 }
 
@@ -30,88 +30,27 @@ bool WASLR::isRequired(){
 }
 
 namespace waslr {
-    void handleFunction(Function &F) {
-      llvm::StringRef funcName = F.getName();
-      if (F.isIntrinsic()){
-        outs() << "Function " << funcName << " Intrinsic! Linkage: " << F.getLinkage() << "\n";
-        return;
-      }
-      if (F.empty()){
-        outs() << "Function " << funcName << " empty! Linkage: " << F.getLinkage() << "\n";
-        return;
-      }
-      if (F.getLinkage() == GlobalValue::AvailableExternallyLinkage) {
-        outs() << "Function " << funcName << " not defined in this Module!\n";
-        return;
-      }
-
-      outs() << "Found Function " << funcName << "Linkage: " << F.getLinkage() << "\n";
-      /*for (BasicBlock &BB : F) {
-        for (Instruction &Inst : BB) {
-          handleInstruction(Inst);
-        }
-      }*/
-  }
 
   /**
    * Notes:
    * - have a look at visitors in the future
    * - MemorySSA could be helpful for data flow analysis
    */
-  void handleInstruction(Instruction &Inst) {
-    // Try casting to Store / Load Instruction and check if not null
-    if (StoreInst *SI = dyn_cast<StoreInst>(&Inst)) {
-      // Write Memory
-      // get Address Space of the pointer Operand
-      unsigned addrspace = SI->getPointerAddressSpace();
-      outs() << "Found Instruction that reads from memory! ADDRSPACE: " << addrspace << "\n";
-    } else if (LoadInst *LI = dyn_cast<LoadInst>(&Inst)) {
-      // Read Memory
-      // get Address Space of the pointer Operand
-      unsigned addrspace = LI->getPointerAddressSpace();
-      outs() << "Found Instruction that writes to memory! ADDRSPACE: " << addrspace << "\n";
-    }
-  }
 
   /**
    * Randomizes the base offset of the stack pointer
    * the stack pointer does not exist in LLVM IR, so we declare it in hopes that wasm-lld will see it and not overwrite
    */
-  void randomizeStackBase(Module &M){
-    outs() << "Randomizing Stack\n";
+  void randomize(Module &M){
     LLVMContext &Ctx = M.getContext();
-    Type *i32Ty = Type::getInt32Ty(Ctx);
-
-    // Declare __stack_pointer as external to be resolved later
-    // Address Space 1 = Wasm globals (https://github.com/emscripten-core/emscripten/issues/12793#issuecomment-915251249)
-    GlobalVariable *stackPtr = new GlobalVariable(M, i32Ty, false, GlobalValue::ExternalLinkage, nullptr, "__stack_pointer", nullptr, GlobalValue::NotThreadLocal, 1);
-    // __stack_pointer may/will come from a different linkage unit
-    stackPtr->setDSOLocal(false);
 
     FunctionType *initFuncTy = FunctionType::get(Type::getVoidTy(Ctx), {}, false);
     // WeakODRLinkage: To merge duplicate functions at link time
-    Function *initFunc = Function::Create(initFuncTy, GlobalValue::WeakODRLinkage, "init_stack_pointer", &M);
-    initFunc->addFnAttr("wasm-export-name", "init_stack_pointer");
+    //Function *initFunc = Function::Create(initFuncTy, GlobalValue::WeakODRLinkage, "init_waslr", &M);
+    //initFunc->addFnAttr("wasm-export-name", "init_waslr");
 
-    BasicBlock *entryBB = BasicBlock::Create(Ctx, "entry", initFunc);
-    IRBuilder<> builder(entryBB);
-
-    // Insert call to print debug message
-    Type *i8PtrTy = PointerType::getInt8Ty(Ctx);
-    FunctionType *consoleFuncTy = FunctionType::get(Type::getVoidTy(Ctx), {i8PtrTy}, false);
-    Function *consoleFunc = M.getFunction("console");
-    if (!consoleFunc) {
-        consoleFunc = Function::Create(consoleFuncTy, GlobalValue::ExternalLinkage, "console", &M);
-    }
-  
-    Constant *debugStr = builder.CreateGlobalStringPtr("Setting stack pointer to 69420");
-    builder.CreateCall(consoleFunc, {debugStr});
-
-    // Randomize stack pointer value (example constant here)
-    uint32_t randOffset = 69420; 
-    Constant *randVal = ConstantInt::get(i32Ty, randOffset);
-    builder.CreateStore(randVal, stackPtr);
-    builder.CreateRetVoid();
+    //randomizeStackBase(M, Ctx, builder);
+    randomizeFunctionStackFrames(M, Ctx);
 
     // Alternative: Use inline assembly to set the stack pointer
     /*std::string asmStr = "i32.const 69420\nglobal.set __stack_pointer";
@@ -128,6 +67,90 @@ namespace waslr {
     //printGlobals(M);
   }
 
+  void debugPrint(Module &M, LLVMContext &Ctx, IRBuilder<> &builder, StringRef msg){
+    // Insert call to print debug message
+    Type *i8PtrTy = PointerType::getInt8Ty(Ctx);
+    FunctionType *consoleFuncTy = FunctionType::get(Type::getVoidTy(Ctx), {i8PtrTy}, false);
+    Function *consoleFunc = M.getFunction("console");
+    if (!consoleFunc) {
+        consoleFunc = Function::Create(consoleFuncTy, GlobalValue::ExternalLinkage, "console", &M);
+    }
+  
+    Constant *debugStr = builder.CreateGlobalStringPtr(msg);
+    builder.CreateCall(consoleFunc, {debugStr});
+  }
+
+  /*
+    Currently, for testing, we insert a global for our random offset that is first imported and then used to generate a random value inside the module. 
+  */
+  void insertRandomValue(Module &M, LLVMContext &Ctx, IRBuilder<> &builder) {
+    Type *i32Ty = Type::getInt32Ty(Ctx);
+    GlobalVariable *waslr_value = new GlobalVariable(M, i32Ty, false, GlobalValue::InternalLinkage, nullptr, "__waslr_value", nullptr, GlobalValue::NotThreadLocal, 1);
+    uint32_t randOffset = 10; // static right now 
+    Constant *randVal = ConstantInt::get(i32Ty, randOffset);
+    Value *curVal = builder.CreateLoad(i32Ty, waslr_value, "waslr_old");
+    Value *newVal = builder.CreateAdd(curVal, randVal, "waslr_new");
+    builder.CreateStore(newVal, waslr_value);
+  }
+
+  void randomizeStackBase(Module &M, LLVMContext &Ctx, IRBuilder<> &builder){
+    outs() << "Randomizing Stack\n";
+    Type *i32Ty = Type::getInt32Ty(Ctx);
+    // Declare __stack_pointer as external to be resolved later
+    // Address Space 1 = Wasm globals (https://github.com/emscripten-core/emscripten/issues/12793#issuecomment-915251249)
+    GlobalVariable *stackPtr = new GlobalVariable(M, i32Ty, false, GlobalValue::ExternalLinkage, nullptr, "__stack_pointer", nullptr, GlobalValue::NotThreadLocal, 1);
+    // __stack_pointer may/will come from a different linkage unit
+    stackPtr->setDSOLocal(false);
+
+    // Randomize stack pointer value (example constant here)
+    uint32_t randOffset = 69420; 
+    Constant *randVal = ConstantInt::get(i32Ty, randOffset);
+    builder.CreateStore(randVal, stackPtr);
+  }
+
+
+  void randomizeFunctionStackFrames(Module &M, LLVMContext &Ctx) {
+    // get stack bounds and stack pointer
+    Type *i32Ty = Type::getInt32Ty(Ctx);
+    PointerType *ptrTy = PointerType::getUnqual(i32Ty);
+
+    FunctionCallee prol_f = M.getOrInsertFunction("__waslr_stack_prol", FunctionType::get(Type::getVoidTy(Ctx), {ptrTy}, false));
+    FunctionCallee epi_f = M.getOrInsertFunction("__waslr_stack_epil", FunctionType::get(Type::getVoidTy(Ctx), {ptrTy}, false));
+      
+    for (Function &F : M) {
+      outs() << "Function: " << F.getName() << "\n";
+      if (F.empty()) continue;
+
+      StringRef Name = F.getName();
+      if (Name == "__waslr_stack_prol" || Name == "__waslr_stack_epil")
+        continue;
+
+      // Instrument entry
+      BasicBlock &entry = F.getEntryBlock();
+      IRBuilder<> builder(&*entry.getFirstInsertionPt());
+
+      AllocaInst *stack_size = builder.CreateAlloca(i32Ty, nullptr, "stack_size");
+
+      builder.CreateCall(prol_f, {stack_size});
+
+      // Instrument return blocks
+      for (BasicBlock &BB: F) {
+        Instruction *term = BB.getTerminator();
+        if(term && isa<ReturnInst>(term)) {
+          IRBuilder<> builder(term);
+          builder.CreateCall(epi_f, {stack_size});
+        }
+      }
+    }
+  }
+
+  int getReadOnlySize(Module &M) {
+    for (auto &GV : M.globals()){
+      // how to do it across modules without LTO?
+      // if one module has X bytes of RO data, while another has Y bytes 
+    }
+  }
+
   // TODO for static data
   void printGlobals(Module &M) {
     
@@ -141,7 +164,6 @@ namespace waslr {
       errs() << "  Linkage: " << GV.getLinkage() << "\n";
       errs() << "  Visibility: " << GV.getVisibility() << "\n";
       errs() << "  Address Space: " << GV.getAddressSpace() << "\n";
-      errs() << " ------ \n";
       for (User *U : GV.users()){
         if (Instruction *I = dyn_cast<Instruction>(U)) {
             errs() << " INST FOUND \n";
@@ -159,6 +181,14 @@ namespace waslr {
           GV.getInitializer()->print(errs());
           errs() << "\n";
       }
+
+      if (GV.hasSection()) {
+        errs() << " Section: " << GV.getSection() << "\n";
+      } else {
+        errs() << "NO Section\n";
+      }
+
+      errs() << " ------ \n\n";
     }
   }
 }
--- waslr_pass/waslr/WASLR.h
+++ waslr_pass/waslr/WASLR.h
@@ -1,5 +1,6 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/IRBuilder.h"
 
 namespace llvm {
 
@@ -14,6 +15,10 @@ namespace llvm {
 namespace waslr {
   void handleFunction(llvm::Function &F);
   void handleInstruction(llvm::Instruction &Inst);
-  void randomizeStackBase(llvm::Module &M);
+  void randomize(llvm::Module &M);
+  void insertRandomValue(llvm::Module &M, llvm::LLVMContext &Ctx, llvm::IRBuilder<> &builder);
+  void randomizeStackBase(llvm::Module &M, llvm::LLVMContext &Ctx, llvm::IRBuilder<> &builder);
+  void randomizeFunctionStackFrames(llvm::Module &M, llvm::LLVMContext &Ctx);
+  void debugPrint(llvm::Module &M, llvm::LLVMContext &Ctx, llvm::IRBuilder<> &builder, llvm::StringRef msg);
   void printGlobals(llvm::Module &M);
 }
